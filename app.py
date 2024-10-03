aa1872762f46c1b7c57ff0ea764d6d5f18db35be
Giridhari Meher <biju.bijumeher@gmail.com>
2024-10-03 12:54:38 +0530

    app.py added

---
+++ app.py
@@ -0,0 +1,9 @@
+n=input("Enter a number:")
+n=int(n)
+def find_survivor(n):
+    if n == 1:
+        return 1
+    else:
+        return (find_survivor(n - 1) + 1) % n + 1
+    print(find_survivor(n)
+          
