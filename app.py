cfff1a456782916c96a36d08b31077d72583d136
Giridhari Meher <biju.bijumeher@gmail.com>
2024-10-03 19:56:14 +0530

    subbranch1 complete

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ def find_survivor(n):
     print(find_survivor(n)
           
 ### it words
+
+## Make stars like a  triangle
+for i in range(5,0,-1):
+    for k in range(1,6-i):
+        print(" ",end="")
+    for j in range (1,(2*i-1)+1):
+        print("*",end="")
+    print("\n")
+    
+    
